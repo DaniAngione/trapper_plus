232462fc7abe6841f1d75605b1f70978c7a893a6
DaniAngione <daniangi@gmail.com>
2019-09-18 11:56:55 -0300

    lots of updates

--- monkey_patches/trapper_plus_trapper.lua
+++ monkey_patches/trapper_plus_trapper.lua
@@ -1,7 +1,13 @@
 local TrapperClass = require 'stonehearth.jobs.trapper.trapper'
-local TrapperPlusClass = class()
-local BaseJob = require 'stonehearth.jobs.crafting_job'
-radiant.mixin(TrapperPlusClass, BaseJob)
-local log = radiant.log.create_logger('trapper_plus')
+local CraftingJob = require 'stonehearth.jobs.crafting_job'
 
+-- Compatibility with ACE
+local AceTrapperClass = require 'stonehearth_ace.monkey_patches.ace_trapper'
+
+local TrapperPlusClass = class()
+	local log = radiant.log.create_logger('trapper_plus')
+	radiant.mixin(TrapperPlusClass, CraftingJob)
+	if AceTrapperClass then
+		radiant.mixin(TrapperPlusClass, AceTrapperClass)
+	end
 return TrapperPlusClass
